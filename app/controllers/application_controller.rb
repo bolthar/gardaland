2693734169f1022bdbeb6d02f09f419b1053ca62
Andrea Dallera <andrea.dallera@toptal.com>
2016-06-01 16:14:35 +0200

    logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  :before_filter log_call
+ 
+   def log_call
+     log = UserLog.new
+     log.ip = request.remote_ip
+     log.route = request.path
+     log.save
+   end
+  
 end
