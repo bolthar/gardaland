93acce0af9d21ff05ceb7965200054cec2badcd8
Andrea Dallera <andrea@bolthar.no>
2018-01-03 13:45:12 +0100

    Revert "Revert "E bulli e offese""
    This reverts commit 1b9c3312912c86e9431ee319b3abbf8356366f95.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :log_call
- 
    def log_call
      log = UserLog.new
      log.ip = request.remote_ip
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150415151452) do
+ActiveRecord::Schema.define(:version => 5) do
 
   create_table "attraction_logs", :force => true do |t|
     t.string   "name"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(:version => 20150415151452) do
     t.integer  "attraction_id"
     t.datetime "date"
     t.integer  "wait_time"
+  end
+
+  create_table "user_logs", :force => true do |t|
+    t.string   "route"
+    t.string   "ip"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
