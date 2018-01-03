51566138e0337d170934f63bde1c602354bbad78
Andrea Dallera <andrea@bolthar.no>
2018-01-03 16:02:45 +0100

    la ggente brutte 2

--- app/assets/javascripts/adachetoest.js
+++ app/assets/javascripts/adachetoest.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   if(window != window.top) {
-    window.location = "https://youporn.com"
+    window.location = "https://www.youporn.com/embed/8663225/danejones-beautiful-woman-with-big-natural-tits-making-love/"
   }
 });
