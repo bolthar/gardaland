53f713343a0d42224bf8bb3de4a7cf9b911fb484
Andrea Dallera <andrea@bolthar.no>
2018-01-03 14:59:51 +0000

    altro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@
 class HomeController < ApplicationController
 
   def index
+    if File.exist?("/tmp/ranked")
+      render :lol and return
+    end
+    @attractions = JsonAttraction.all
   end
 
   def adachetoest
